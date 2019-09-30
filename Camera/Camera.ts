616e34ebeaba18444a764d42a469cb9bde81e371
PrezesHimself <mateuszrorat@gmail.com>
2019-09-30 23:34:40 +0200

    Camera movement

--- Camera/Camera.ts
+++ Camera/Camera.ts
@@ -7,10 +7,16 @@ import {Keyboard} from "../Input/Keyboard";
 export class Camera extends Entity {
   private freeHand: boolean = true;
   private isColliding: boolean = false;
+  weight: number = 60 * 0.017;
+  speed = {
+    x: .4,
+    y: .4
+  }
 
   constructor(
     public position: Point,
     private readonly target: Entity = null,
+    protected velocity: Vector2D = {x: 0, y: 0},
     private readonly canvas: HTMLCanvasElement 
   ) {
     
@@ -18,6 +24,7 @@ export class Camera extends Entity {
   }
 
   update(input: Input, world: World) {
+
     if(input.keyboard.isDown(Keyboard.KEYCODES.f)) {
       this.freeHand = false
     } 
@@ -26,16 +33,31 @@ export class Camera extends Entity {
       this.freeHand = true
     } 
 
+    if(input.keyboard.isDown(Keyboard.KEYCODES.d)) {
+      this.velocity.x += this.speed.x;
+    } 
+    if(input.keyboard.isDown(Keyboard.KEYCODES.a)) {
+      this.velocity.x -= this.speed.x;
+    } 
+    if(input.keyboard.isDown(Keyboard.KEYCODES.w)) {
+      this.velocity.y -= this.speed.x;
+    }  
+    if(input.keyboard.isDown(Keyboard.KEYCODES.s)) {
+      this.velocity.y += this.speed.x;
+    }
+
     if(this.target && this.freeHand) {
       this.position.x = this.target.position.x;
       this.position.y = this.target.position.y;
+    } else {
+      super.update(input, world);
     }
   }
 
   render(context: CanvasRenderingContext2D ) {
     context.translate(
-      -this.position.x + (this.canvas.width/2),
-      -this.position.y + (this.canvas.height/2)
+      -this.position.x ,
+      -this.position.y 
     );
   }
 }
